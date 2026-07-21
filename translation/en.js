ceb543241b9a6c4b16baec999997017d5854ea0f
Mcpasi <kuspas29@gmail.com>
2026-07-21 23:58:50 +0200

    Add 'new' translation key to en.js

--- translation/en.js
+++ translation/en.js
@@ -3,5 +3,6 @@ const i18n_en = {
   your: "Your agent at work",
   description: "Codex IDE is an agent-based IDE with a real-time editor and console. Watch your agent at work live in real time, all under your control.",
   start: "Start now", 
-  what: "What can she do?"
+  what: "What can she do?",
+  new: "New file"
 };
